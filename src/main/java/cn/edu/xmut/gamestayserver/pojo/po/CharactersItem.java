89410ad6122eb344d83cec892837be5f8ab1bd82
qiuguanlin <578482558@qq.com>
2019-03-31 19:08:52 +0800

    CharactersItem表结构修改

--- src/main/java/cn/edu/xmut/gamestayserver/pojo/po/CharactersItem.java
+++ src/main/java/cn/edu/xmut/gamestayserver/pojo/po/CharactersItem.java
@@ -1,7 +1,10 @@
 package cn.edu.xmut.gamestayserver.pojo.po;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
+@Data
 @Table(name = "characters_item")
 public class CharactersItem {
     @Id
@@ -19,87 +22,6 @@ public class CharactersItem {
     @Column(name = "come_from")
     private String comeFrom;
 
-    /**
-     * @return id
-     */
-    public Integer getId() {
-        return id;
-    }
+    private String url;
 
-    /**
-     * @param id
-     */
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    /**
-     * @return profile
-     */
-    public String getProfile() {
-        return profile;
-    }
-
-    /**
-     * @param profile
-     */
-    public void setProfile(String profile) {
-        this.profile = profile == null ? null : profile.trim();
-    }
-
-    /**
-     * @return name
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * @param name
-     */
-    public void setName(String name) {
-        this.name = name == null ? null : name.trim();
-    }
-
-    /**
-     * @return type1
-     */
-    public String getType1() {
-        return type1;
-    }
-
-    /**
-     * @param type1
-     */
-    public void setType1(String type1) {
-        this.type1 = type1 == null ? null : type1.trim();
-    }
-
-    /**
-     * @return type2
-     */
-    public String getType2() {
-        return type2;
-    }
-
-    /**
-     * @param type2
-     */
-    public void setType2(String type2) {
-        this.type2 = type2 == null ? null : type2.trim();
-    }
-
-    /**
-     * @return come_from
-     */
-    public String getComeFrom() {
-        return comeFrom;
-    }
-
-    /**
-     * @param comeFrom
-     */
-    public void setComeFrom(String comeFrom) {
-        this.comeFrom = comeFrom == null ? null : comeFrom.trim();
-    }
 }
